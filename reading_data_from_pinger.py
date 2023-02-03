37d329a525818bf44550e47f59cfe7e0f62063d6
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2023-02-03 16:40:16 -0600

    Add files via upload

---
+++ reading_data_from_pinger.py
@@ -0,0 +1,20 @@
+import time
+import csv
+
+from brping import Ping1D
+myPing = Ping1D()
+myPing.connect_serial("COM3", 115200)
+if myPing.initialize() is False:
+    print("Failed to initialize Ping!")
+    exit(1)
+myPing.set_speed_of_sound(1500000,verify = True)
+
+with open('Pinger_data_file1','w',newline = '\n') as file:
+    writer = csv.writer(file,delimiter = ',')
+    writer.writerow(['Pinger distance'])
+    while True:
+        data = myPing.get_distance_simple()
+        distance_meters = data['distance'] * (10 ** -3)
+        writer.writerow(distance_meters)
+        time.sleep(1)
+
