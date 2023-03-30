6a9be47ac4b098a209e5bf7e2a8b33fde33d8e25
reafauv2017 <reaf.tamu@gmail.com>
2023-03-30 14:59:38 -0500

    Updated Servo code with Python Objects

--- ThrusterTest/servo_test.py
+++ ThrusterTest/servo_test.py
@@ -15,17 +15,50 @@ M3 = kit.servo[10].angle = 90
 M4 = kit.servo[11].angle = 90    
 
 
-while(1):
-	kit.servo[9].angle = 90
-	print(90)
-	time.sleep(2)
-
-	#kit.servo[9].angle = 92
-	#print(94)
-	#time.sleep(5)
-
 '''import RPi.GPIO as GPIO
 import timeGPIO.setmode(GPIO.BOARD)
 GPIO.setup(15,GPIO.OUT)
 
 servo.angle == 90'''
+def set_speed(speed, motor):
+	kit.servo[motor].angle = speed
+	return
+
+class Motor:
+	def __init__(self, name):
+		self.name = name
+		self.speed = 90
+		self.prev_speed = self.speed
+	def setSpeed(self, speed):
+		self.speed = speed
+
+	def run(self):
+		if self.prev_speed != self.speed:
+			print("boop")
+			kit.servo[self.name].angle = self.speed
+			self.prev_speed = self.speed
+		else:
+			return
+	def stop(self):
+		kit.servo[name] = 90
+
+A1 = Motor(0)
+A2 = Motor(1)
+A3 = Motor(2)
+A4 = Motor(3)
+M1 = Motor(7)
+M2 = Motor(9)
+M3 = Motor(10)
+M4 = Motor(11)
+
+#kit.servo[0].angle = 95
+while(1):
+
+	A1.setSpeed(90)
+	A1.run()	
+	print(A1.speed)
+	time.sleep(2)
+
+	#kit.servo[0].angle = 87
+	#print(94)
+	#time.sleep(5)
