aca68cc7a39e0d12351e1e0abb04ebd2f549e0ac
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2023-06-03 12:52:33 -0500

    Update VectorNavPublisher.py

--- VectorNavPublisher.py
+++ VectorNavPublisher.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float32
+from vnpy import *
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
-def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("chatter", Float32, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
-if __name__ == '__main__':
-    listener()
-
+s = VnSensor()
+s.connect(‘/dev/ttyUSB0’,115200)
+ 
+def sender():
+     pub = rospy.Publisher('Vnav', Float32, queue_size=10)
+     rospy.init_node('vector_nav', anonymous=True)
+     rate = rospy.Rate(5) # 10hz
+     while not rospy.is_shutdown():
+         sensor = s.read_imu_measurements()
+         data_pressure = sensor.pressure
+         rospy.loginfo(data_pressure)
+         pub.publish(data_pressure)
+         rate.sleep()
+         
+         data_gyro = sensor.gyro
+         rospy.loginfo(data_gyro)
+         pub.publish(data_gyro)
+         rate.sleep()
+ 
+ if __name__ == '__main__':
+     try:
+         sender()
+     except rospy.ROSInterruptException:
+         pass
