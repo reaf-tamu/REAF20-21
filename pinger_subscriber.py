48a5d4eb3ea1d20c44b3af23e3734682de78705a
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2023-06-03 12:54:25 -0500

    Update pinger_subscriber.py

--- pinger_subscriber.py
+++ pinger_subscriber.py
@@ -1,27 +1,19 @@
-#!/usr/bin/env python3
-import rospy
-#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float32
 
-
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
+    rospy.loginfo("Recieved Data: %f", data.data)
 
 def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("chatter", Float32, callback)
+    rospy.init_node('Subscriber_Node', anonymous=True)
+    rospy.Subscriber("Pinger", Float32, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 
 if __name__ == '__main__':
+  try:
     listener()
+  except rospy.ROSInterruptException:
+    pass
