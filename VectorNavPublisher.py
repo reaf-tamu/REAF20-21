348731a00d67e69520e2140e8adbd0d7aa564405
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2023-06-17 18:21:11 -0500

    Update VectorNavPublisher.py

--- VectorNavPublisher.py
+++ VectorNavPublisher.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Float32
+#from std_msgs.msg import Float32
+from std_msgs.msg import String
 from vnpy import *
 
 s = VnSensor()
 s.connect(‘/dev/ttyUSB0’,115200)
  
 def sender():
-     pub = rospy.Publisher('Vnav', Float32, queue_size=10)
+     pub = rospy.Publisher('Vnav', String, queue_size=10)
      rospy.init_node('vector_nav', anonymous=True)
      rate = rospy.Rate(5) # 10hz
      while not rospy.is_shutdown():
+#          sensor = s.read_imu_measurements()
+#          data_pressure = sensor.pressure
+#          rospy.loginfo(data_pressure)
+#          pub.publish(data_pressure)
+#          rate.sleep()
+         
+#          data_gyro = sensor.gyro
+#          rospy.loginfo(data_gyro)
+#          pub.publish(data_gyro)
+#          rate.sleep()
+         orientation = s.read_yaw_pitch_roll()
          sensor = s.read_imu_measurements()
          data_pressure = sensor.pressure
-         rospy.loginfo(data_pressure)
-         pub.publish(data_pressure)
+         rospy.loginfo("Reading vNav now: ", orientation.x, orientation.y, orientation.z, data_pressure)
+         msg = orientation.x + " " + orientation.y + " " + orientation.z + " " + data_pressure 
+         pub.publish(msg)
          rate.sleep()
          
-         data_gyro = sensor.gyro
-         rospy.loginfo(data_gyro)
-         pub.publish(data_gyro)
-         rate.sleep()
+     
  
  if __name__ == '__main__':
      try:
