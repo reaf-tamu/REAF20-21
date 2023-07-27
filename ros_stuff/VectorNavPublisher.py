c8f5352332cd4555ef1f5cd8a86a6b91da207fc6
alexandria-joli <126917974+alexandria-joli@users.noreply.github.com>
2023-07-26 19:55:44 -0500

    Update VectorNavPublisher.py
    Cleans up and comments for competition

--- ros_stuff/VectorNavPublisher.py
+++ ros_stuff/VectorNavPublisher.py
@@ -1,68 +1,65 @@
 #!/usr/bin/env python3
 import rospy
-#from std_msgs.msg import Float32
-from std_msgs.msg import String
+from std_msgs.msg import String, Float 32
 from vnpy import *
 from math import atan2, pi
 import time
-import Jetson.GPIO as GPIO
 
 
-# Set the GPIO pin numbering mode
-GPIO.setmode(GPIO.BOARD)
-# Specify the GPIO pin number you want to read from
-pin_number = 18
-# Set up the GPIO pin as an input
-GPIO.setup(pin_number, GPIO.IN)
-
-
-mission_switch = False
-while mission_switch == False: #checking if mission switch is on
-    input_status = GPIO.input(pin_number)
-    print(input_status)
-    if input_status == GPIO.LOW:
-        mission_switch = True #break out of while loop and continue on to the rest of the code
-    time.sleep(1)
-
-
+# connect to sensor
 s = VnSensor()
 s.connect("/dev/ttyUSB0",115200)
- 
+
+
+# the code part of it
 def sender():
 	pub = rospy.Publisher('Vnav', String, queue_size=10)
 	rospy.init_node('vector_nav', anonymous=True)
-	# rate = rospy.Rate(5) # 10hz
 	while not rospy.is_shutdown():
-#		sensor = s.read_imu_measurements()
-#		data_pressure = sensor.pressure
-#		rospy.loginfo(data_pressure)
-#		pub.publish(data_pressure)
-#		rate.sleep()
-#		data_gyro = sensor.gyro
-#		rospy.loginfo(data_gyro)
-#		pub.publish(data_gyro)
-#		rate.sleep()
+		# reads the sensors
 		orientation = s.read_yaw_pitch_roll()
-#		x = orientation.x
-#		y = orientation.y
-#		z = orientation.z
 		sensor = s.read_imu_measurements()
 		data_pressure = sensor.pressure
 		heading = atan2(sensor.mag.x, sensor.mag.y) * 180 / pi
+
+		# sets the variables that will be printed and formats to a string
+		# string was the best way we could find to be compatible with ROS
 		msg = orientation.x, orientation.y, orientation.z, data_pressure, heading, heading-80
 		data = str(msg)
-#		data = "X:" + str(x) + ", Y:" + str(y) + ", Z:" + str(z) + ", Pressure:" + str(data_pressure) + ", Heading:" + str(heading)
+
+		# "prints" the data to the terminal
 		rospy.loginfo(data)
-#		rospy.loginfo("Reading vNav now: ", orientation.x, orientation.y, orientation.z, data_pressure)
-#		msg = orientation.x + " " + orientation.y + " " + orientation.z + " " + data_pressure
+
+		# essentially sends the data to subscriber
 		pub.publish(data)
-		# rate.sleep()
 		time.sleep(1)
-         
-     
- 
+
+
+# actually runs all the code above
 if __name__ == '__main__':
 	try:
 		sender()
 	except rospy.ROSInterruptException:
 		pass
+
+
+# stuff that can be added to the function
+"""
+		# if you only wanted pressure
+		rospy.loginfo(data_pressure)
+		pub.publish(data_pressure)
+
+  		# an attempt at header, but header worked better
+		data_gyro = sensor.gyro
+		rospy.loginfo(data_gyro)
+		pub.publish(data_gyro)
+
+  		# others ways to format our list of data
+    		# the one in the code works best and is easiest to parse through in the subscriber
+		x = orientation.x
+		y = orientation.y
+		z = orientation.z
+		data = "X:" + str(x) + ", Y:" + str(y) + ", Z:" + str(z) + ", Pressure:" + str(data_pressure) + ", Heading:" + str(heading)
+		rospy.loginfo("Reading vNav now: ", orientation.x, orientation.y, orientation.z, data_pressure)
+		msg = orientation.x + " " + orientation.y + " " + orientation.z + " " + data_pressure
+  """
