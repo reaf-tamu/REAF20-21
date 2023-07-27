9e696a4a290b6d919482ef2415b9fe82a2a69ad2
alexandria-joli <126917974+alexandria-joli@users.noreply.github.com>
2023-07-26 20:18:25 -0500

    Update publisher_csv.py
    Cleaned and commented

--- ros_stuff/publisher_csv.py
+++ ros_stuff/publisher_csv.py
@@ -4,7 +4,31 @@ from std_msgs.msg import Float32, String
 import csv
 import time
 
-# vectornav csv
+
+# ROS would not work with pressure sensor due to library issues
+# work around by having python file write to csv file, publisher reads it and publishes the values
+def talker():
+	pub = rospy.Publisher("Pressure", String, queue_size=10)
+	rospy.init_node("pressure_sensor", anonymous = True)
+	while not rospy.is_shutdown():
+		with open('/home/reafauv2017/Desktop/auv/REAF20-21/bar02/pressure_sensor_pub.csv', 'r') as file: # change path
+			reader =  csv.reader(file)
+			for row in reader:
+				data = str(row)
+				rospy.loginfo(data) # prints it
+				pub.publish(data) # publishes it
+		# the sleep is mainly there to close the file, the csv cannot be opened in two places
+		time.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
+
+# originally tested publisher csv functionality with vnav
 """
 def talker():
 	pub = rospy.Publisher("CSV", String, queue_size=10)
@@ -18,23 +42,3 @@ def talker():
 				pub.publish(data)
 		time.sleep(10)
 """
-
-# pressure sensor csv
-# need to connect pressure sensor
-def talker():
-	pub = rospy.Publisher("Pressure", String, queue_size=10)
-	rospy.init_node("pressure_sensor", anonymous = True)
-	while not rospy.is_shutdown():
-		with open('/home/reafauv2017/Desktop/auv/REAF20-21/bar02/pressure_sensor_pub.csv', 'r') as file:
-			reader =  csv.reader(file)
-			for row in reader:
-				data = str(row)
-				rospy.loginfo(data)
-				pub.publish(data)
-		time.sleep(1)
-
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
